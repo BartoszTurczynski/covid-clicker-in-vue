42d0c065bc4276d6d39d41d6ba4377afb93f31d0
bart016 <bart.turczynski@gmail.com>
2022-01-26 13:26:37 +0100

    navbar 1.0

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,13 +1,26 @@
 <template>
-<div>witam we vue</div>
+<div class="main">
+  <Navbar />
+    witam we vue
+  witam we vue
+  witam we vue
+  </div>
 </template>
-
 <script>
-
+import Navbar from './components/Navbar.vue'
+export default{
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
 </script>
 
 <style>
 #app {
+  margin: 0;
+  padding: 0;
+  margin-top: 8em;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ project/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="navbar">
+        <ul>
+            <li>COVID CLICKER</li>
+            <li>O NAS</li>
+            <li>POBIERZ</li>
+        </ul>
+        <img src="../assets/logo.svg" alt="covid">
+    </div>
+</template>
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+<style scoped>
+.navbar {
+    left: 0;
+    background-color: red;
+    height: 10vh;
+    top: 0;
+    position: fixed;
+    padding: 0em;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+}
+ul{
+    
+    list-style: none;
+    
+}
+ul li{
+    cursor: pointer;
+    color: black;
+}
+img{
+    height: 100px;
+    background-color: white;
+    border-radius: 50%;
+}
+</style>
