c50072ac8d1d7f8a3cc64fb451049ad7e7a59887
bart016 <bart.turczynski@gmail.com>
2022-01-26 12:25:31 +0100

    initial commit

---
+++ project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>witam we vue</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
